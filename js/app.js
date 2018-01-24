90321feb9e6c3caf8032ed0064de25201f9b1b12
Fahri Tardif <fahri.tardif@toumoro.com>
2018-01-24 15:52:06 -0500

    work in progress -- commiting to affect some changes on master

--- js/app.js
+++ js/app.js
@@ -1,3 +1,67 @@
 "use strict";
-
 $(document).foundation();
+
+var content = "";
+
+// Orc object simple constructor
+function Orc(lastName){  
+    var firstName = setOrcName();
+    var lastName = lastName ? lastName : setOrcName();
+    
+    // ! Arrow syntax 1 : Shorter syntax
+    this.getFullName = () => firstName + ' ' + lastName; // getter for the Orc full name
+    // Non arrow equivalent
+    // this.getFullName = function(){
+    //     return this.firstName + ' ' + this.lastName;
+    // };    
+};
+
+/**
+ * ! Functions and variables are hoisted : 
+ * Hoisting is a JavaScript mechanism where variables and function declarations are moved to the top of their scope before code execution.
+ * @see https://developer.mozilla.org/en-US/docs/Glossary/Hoisting
+ */
+
+// Return a random name
+function setOrcName(){
+    let rand = getRandomInt(5,2);
+    let name = '';
+    for(let i=0; i < rand; i++){
+        name += getConsonantVowelPair();
+    }
+    return name.charAt(0).toUpperCase() + name.slice(1);
+};
+
+
+// MAIN
+function main(){
+    // global array to contain orcs
+    var orcArmy = [];
+
+    content += "<p> Let's create some orcs </p>";
+    for(let i=0; i < armySize; i++){
+        orcArmy[i] = new Orc(); 
+    }
+    
+    // Instantiating the variable "orc" with let
+    let orc = orcArmy[0];
+    
+    // Reusing the same let variable "orc" to loop through orcs -- let limits this "orc" variable scope's to this block
+    // for ... of simple loop : The for...of statement creates a loop iterating over iterable objects 
+    for(let orc of orcArmy){
+        content +=  '<div class="card" style="width: 300px;"><div class="card">Orc</div>' 
+            + '<img src="img/grunt.png" style="max-width: 100%;"><div class="card-section">'
+            + '<h4>' + orc.getFullName() + '</h4></div></div>';
+    }
+
+    // The orc variable is still containing the first orc of the orcArmy array.
+    content += "<p> The first orc is : " + orc.getFullName() + "</p>";
+
+    // Write content to the browser
+    document.getElementById('content').innerHTML = content;
+}
+
+// Equivalent to jQuery $(document).ready() in vanilla js
+document.addEventListener("DOMContentLoaded", function() {
+     main();
+});
--- js/util.js
+++ js/util.js
@@ -1,1 +1,18 @@
 "use strict";
+
+// constants
+const consonant = ['b', 'd', 'f', 'g', 'k', 't', 'p', 'z'];
+const vowel = ['a', 'e', 'i', 'o', 'u', 'y'];
+const armySize = 3;
+
+/************** utils ***************/
+
+// Return a random integer between min (included) & max (excluded)
+function getRandomInt(max, min=0){
+    return Math.floor(Math.random() * (max - min)) + min;
+};
+
+// Return a concatenated pair of consonant-vowel
+function getConsonantVowelPair(){
+    return consonant[ getRandomInt(consonant.length) ] + vowel[ getRandomInt(vowel.length) ];
+};
