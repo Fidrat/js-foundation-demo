76628f1af69eda4a7cae73b79d7999932117f12c
Fahri Tardif <fahri.tardif@toumoro.com>
2018-01-26 10:42:00 -0500

    commented util and added a getRandomArrayValue function

--- js/util.js
+++ js/util.js
@@ -10,12 +10,29 @@ const orcCardHtmlEnd = '</div></div>';
 
 /************** utils ***************/
 
-// Return a random integer between min (included) & max (excluded)
+/**
+ * Return a random integer between min (included) & max (excluded)
+ * @param max int
+ * @param min  int
+ * @returns int
+ */ 
 function getRandomInt(max, min=0){
     return Math.floor(Math.random() * (max - min)) + min;
 };
 
-// Return a concatenated pair of consonant-vowel
+/**
+ * Return a random value from array
+ * @param arr array
+ * @returns mixed array value
+ */
+function getRandomArrayValue(arr){
+    return (arr.constructor === Array) ? arr[Math.floor(Math.random() * arr.length)] : false;
+}
+
+/**
+ * Return a concatenated pair of consonant-vowel
+ * @returns {String}
+ */ 
 function getConsonantVowelPair(){
     return consonant[ getRandomInt(consonant.length) ] + vowel[ getRandomInt(vowel.length) ];
 };
